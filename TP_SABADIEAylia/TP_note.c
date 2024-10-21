b4bac0f51bd0aeb09e034bc3398583ff5218a9b9
Aylia <aylia.sabadie@student.junia.com>
2024-10-21 10:50:16 +0200

    L'exercice 2 est terminé.

--- TP_SABADIEAylia/TP_note.c
+++ TP_SABADIEAylia/TP_note.c
@@ -13,20 +13,32 @@ int initialisation(int grille[9][9], int taille) {
 	return 0;
 }
 
+int remplir(int grille[9][9], int taille) {
+	int ligne = 0;
+	int colonne = 0;
+	int valeur = 0;
+	int continuer = 1;
+	while (continuer == 1)
+	{
+		printf("\nQuelle ligne souhaitez-vous modifier? ");
+		scanf_s("%d", &ligne);
+		printf("\nQuelle colonne souhaitez-vous modifier? ");
+		scanf_s("%d", &colonne);
+		printf("\nQuelle est votre valeur? ");
+		scanf_s("%d", &valeur);
+		if (ligne <= taille && colonne <= taille)
+		{
+			grille[ligne-1][colonne-1] = valeur;
+		}
+		printf("\nSouhaitez-vous modifier une autre valeur? (oui - 1 / non - 0) ");
+		scanf_s("%d", &continuer);
+	}
+	return 0;
+}
+
 int main()
 {
-	int grille_facile[9][9] = {
-		{6, 0, 4, 9, 0, 0, 3, 0, 8},
-		{3, 2, 0, 8, 0, 0, 4, 0, 0},
-		{0, 0, 0, 6, 0, 4, 5, 2, 1},
-		{1, 0, 2, 7, 4, 3, 8, 9, 6},
-		{8, 0, 0, 0, 0, 0, 0, 0, 0},
-		{9, 4, 7, 5, 8, 0, 0, 0, 0},
-		{4, 9, 0, 2, 0, 8, 6, 0, 3},
-		{0, 3, 6, 4, 0, 0, 0, 0, 0},
-		{0, 7, 0, 0, 6, 9, 0, 0, 4}
-	};
-
+	
 	int grille_moyenne[9][9] = {
 		{1, 0, 0, 7, 0, 6, 0, 0, 0},
 		{0, 0, 0, 0, 0, 8, 5, 6, 0},
@@ -39,31 +51,27 @@ int main()
 		{8, 4, 0, 3, 0, 1, 0, 5, 9}
 	};
 
-	int taille = 3;
-	initialisation(grille_facile, taille);
-	for (int i = 0; i < taille; i++)
-	{
-		for (int j = 0; j < taille; j++)
-		{
-			printf("%d", grille_facile[i][j]);
-		}
-		printf("\n");
-	}
-
+	int taille = 9;
 	initialisation(grille_moyenne, taille);
 	for (int i = 0; i < taille; i++)
 	{
-		printf("\n");
 		for (int j = 0; j < taille; j++)
 		{
 			printf("%d", grille_moyenne[i][j]);
 		}
+		printf("\n");
 	}
 
-	return 0;
+	remplir(grille_moyenne, taille);
 
-
-
+	for (int i = 0; i < taille; i++)
+	{
+		for (int j = 0; j < taille; j++)
+		{
+			printf("%d", grille_moyenne[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
